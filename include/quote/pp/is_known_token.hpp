fa86971516c084c859559d050b74261404d0d213
quartorz <tsuhaso@gmail.com>
2016-04-03 02:41:52 +0900

    add comment

--- include/quote/pp/is_known_token.hpp
+++ include/quote/pp/is_known_token.hpp
@@ -4,6 +4,14 @@
 #include <quote/pp/is_empty.hpp>
 #include <quote/pp/cat.hpp>
 
+/*
+
+"token"という名前のトークンを登録するには
+#define QUOTE_PP_TOKEN_token ,
+とする
+
+*/
+
 #if !defined(QUOTE_PP_IS_KNOWN_TOKEN)
 #  define QUOTE_PP_IS_KNOWN_TOKEN(token) \
 	QUOTE_PP_IS_KNOWN_TOKEN_I(QUOTE_PP_TUPLE_ELEMENT(0, (QUOTE_PP_CAT(QUOTE_PP_TOKEN_, token))))
