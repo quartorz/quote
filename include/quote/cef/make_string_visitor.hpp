b1feeab6821bdfaadb79280f385e45e9f0019ee1
quartorz <tsuhaso@gmail.com>
2016-09-27 17:53:01 +0900

    Add cef::make_string_visitor

---
+++ include/quote/cef/make_string_visitor.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "include/cef_browser.h"
+
+namespace quote{ namespace cef{
+
+	namespace detail{
+		template <typename Func>
+		class StringVisitor : public CefStringVisitor {
+			IMPLEMENT_REFCOUNTING(StringVisitor);
+
+			Func f_;
+
+		public:
+			StringVisitor(Func f)
+				: f_(f)
+			{
+			}
+
+			void Visit(const CefString &s) override
+			{
+				f_(s);
+			}
+		};
+	}
+
+	template <typename Func>
+	CefRefPtr<detail::StringVisitor<Func>> make_string_visitor(Func f)
+	{
+		return new detail::StringVisitor<Func>(f);
+	}
+
+} }
